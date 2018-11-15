6ac9942c0a4d0aafdaa197c8c13a491243838139
Jordan Obed Acosta Bernal <Jordan_Acosta@bvc.com.co>
2018-11-15 18:52:23 -0500

    second commit

--- codigo.js
+++ codigo.js
@@ -1,5 +1,5 @@
 
-alert("Hola mundo");
+alert("Hola mundo jordan");
 var ruta = window.location;
 console.log(ruta);
 document.write("Estas en: " + ruta );
--- dibujito.js
+++ dibujito.js
@@ -1,11 +1,39 @@
 /*alert("Sachita es magia")*/
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var l = 0;
+var lineas = 30;
 console.log(lienzo);
+var yi, xf;
+
+dibujarlinea("grey", 0, 0, 10, 300);
+dibujarlinea("grey", 0, 10, 20, 300)
 
 
+while(l < lineas)
+{
+yi = 10 * l;
+xf = 10 * (1 + l);
+dibujarlinea("grey", 0, yi, xf, 300);
+l++;
+console.log("Linea" +l);
+}
 
-lienzo.beginPath();
+dibujarlinea("red", 1, 1, 1, 300);
+dibujarlinea("red", 1, 299, 299, 299);
+
+function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
+
+
+/*lienzo.beginPath();
 lienzo.strokeStyle = "red";
 lienzo.moveTo(300, 300);
 lienzo.lineTo(1, 300);
@@ -17,4 +45,4 @@ lienzo.strokeStyle = "red";
 lienzo.moveTo(1, 300);
 lienzo.lineTo(1, 1);
 lienzo.stroke();
-lienzo.closePath();
+lienzo.closePath();*/
